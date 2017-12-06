09b2571f19ca42732462c415b5aa61a48d9ecce7
lab <rs_lidn@163.com>
2017-12-06 22:27:15 +0800

    load data

--- netmodule/loadimg.py
+++ netmodule/loadimg.py
@@ -1,5 +1,5 @@
-import sys
-sys.path.append('/media/flag54/54368BA9368B8AA6/DataSet/coco/cocoapi-master/PythonAPI/')
+# import sys
+# sys.path.append('/home/flag54/Downloads/coco/cocoapi-master/PythonAPI/')
 
 from pycocotools.coco import COCO
 import numpy as np
@@ -7,31 +7,23 @@ from scipy.misc import imread
 import matplotlib.pyplot as plt
 import pylab
 
-dataDir = '/media/flag54/54368BA9368B8AA6/DataSet/coco/'
+dataDir = '/home/flag54/Downloads/coco/'
 dataType = 'val2017'
-annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
+annFile = '{}/annotations/person_keypoints_{}.json'.format(dataDir, dataType)
 
-# initialize COCO api for instance annotations
+# initialize COCO api for person_keypoints_ annotations
 coco = COCO(annFile)
-
 # display COCO categories and supercategories
 cats = coco.loadCats(coco.getCatIds())
-nms = [cat['name'] for cat in cats]
-print('COCO categories: \n{}\n'.format(' '.join(nms)))
 
 nms = set([cat['supercategory'] for cat in cats])
 print('COCO supercategories: \n{}'.format(' '.join(nms)))
 
-# get all images containing given categories, select one at random
-# catIds = coco.getCatIds(catNms=['person','dog','skateboard']);
-# imgIds = coco.getImgIds(catIds=catIds );
-imgIds = coco.getImgIds(imgIds=[324158])
-img = coco.loadImgs(imgIds[np.random.randint(0, len(imgIds))])[0]
+catID = coco.getCatIds(catNms=['person'])
+imgID = coco.getImgIds(catIds=catID)
+idx = np.random.randint(0, len(imgID))
+img = coco.loadImgs(imgID[idx])[0]
+img_path = '%simages/%s' % (dataDir, img['file_name'])
+data = imread(img_path)
+annID=coco.getAnnIds(imgIds=img['id'])
 
-# load and display image
-# I = imread('%s/images/%s/%s'%(dataDir,dataType,img['file_name']))
-# use url to load image
-I = imread(img['coco_url'])
-plt.axis('off')
-plt.imshow(I)
-plt.show()
